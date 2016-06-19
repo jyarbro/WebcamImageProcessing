9257d16bbe80deacd2c25cac64cd9370e264e9e3
James Yarbro <yarbro@outlook.com>
2016-06-19 21:35:26 +0200

    removed unused files.

--- KIP2/Models/Neuron.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace KIP2.Models {
-	public class Neuron {
-		public int signature;
-		public object Memory;
-		public Dictionary<Neuron, int> Weights;
-	}
-}
