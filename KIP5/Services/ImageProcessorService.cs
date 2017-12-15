f2b4d4ee63713f01be90108e32fae54f0633f657
Jim Yarbro <yarbro@outlook.com>
2017-12-15 21:39:25 +0100

    Copied KIP4 into KIP5 in prep for changes.

---
+++ KIP5/Services/ImageProcessorService.cs
@@ -0,0 +1,274 @@
+﻿using KIP.Structs;
+using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace KIP5.Services {
+	public class ImageProcessorService {
+		const int THRESHOLD = 128 * 3;
+		const int FOCUSPARTWIDTH = 11;
+
+		public WriteableBitmap OutputImage { get; } = new WriteableBitmap(1920, 1080, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+		KinectBuffer ColorBuffer;
+		Int32Rect FrameChangedRect;
+		Pixel[][] OverlayLayers;
+		Pixel[] Pixels;
+		int FrameWidth;
+		int FrameStride;
+		uint PixelCount;
+		int[] FocusPartOffsets;
+		int[] EdgeFilterWeights;
+		int[] EdgeFilterOffsets;
+		byte[] ColorFrameData;
+		byte[] OutputData;
+
+		int _i;
+		int _j;
+		Pixel _pixel;
+		int _pixelValue;
+
+		public void UpdateInput(ColorFrame colorFrame) {
+			if (colorFrame.FrameDescription.Width != OutputImage.PixelWidth || colorFrame.FrameDescription.Height != OutputImage.PixelHeight)
+				return;
+
+			using (ColorBuffer = colorFrame.LockRawImageBuffer()) {
+				// Do I need a lock?
+				lock(ColorFrameData) {
+					colorFrame.CopyConvertedFrameDataToArray(ColorFrameData, ColorImageFormat.Bgra);
+				}
+			}
+
+			// I had a try-catch here. why??
+		}
+
+		public void UpdateOutput() {
+			CopyFrameData();
+			ResetOutputToBaseline();
+			ApplyFilter();
+
+			Application.Current?.Dispatcher.Invoke(() => {
+				OutputImage.Lock();
+				OutputImage.WritePixels(FrameChangedRect, OutputData, FrameStride, 0);
+				//OutputImage.AddDirtyRect(FrameChangedRect);
+				OutputImage.Unlock();
+			});
+		}
+
+		unsafe void CopyFrameData() {
+			_i = 0;
+
+			fixed (Pixel* pixels = Pixels) {
+				fixed (byte* inputData = ColorFrameData) {
+					var pixel = pixels;
+					var color = inputData;
+
+					while (_i++ < PixelCount) {
+						pixel->B = *(color);
+						pixel->G = *(color + 1);
+						pixel->R = *(color + 2);
+
+						color += 4;
+						pixel++;
+					}
+				}
+			}
+		}
+
+		unsafe void ResetOutputToBaseline() {
+			fixed (Pixel* pixels = Pixels) {
+				fixed (byte* outputData = OutputData) {
+					var pixel = pixels;
+					var outputByte = outputData;
+					_i = 0;
+
+					while (_i++ < PixelCount) {
+						*(outputByte) = pixel->B;
+						*(outputByte + 1) = pixel->G;
+						*(outputByte + 2) = pixel->R;
+
+						pixel++;
+						outputByte += 4;
+					}
+				}
+			}
+		}
+
+		unsafe void ApplyFilter() {
+			fixed (byte* outputData = OutputData) {
+				var outputBytePtr = outputData;
+				_i = 0;
+
+				while (_i++ < PixelCount) {
+					var totalEffectiveValue = 0;
+
+					for (var edgeFilterIndex = 0; edgeFilterIndex < EdgeFilterOffsets.Length; edgeFilterIndex++) {
+						_j = _i + EdgeFilterOffsets[edgeFilterIndex];
+
+						if (_j < 0 || _j >= PixelCount)
+							continue;
+
+						_pixel = Pixels[_j];
+						_pixelValue = (_pixel.B + _pixel.G + _pixel.R) * EdgeFilterWeights[edgeFilterIndex];
+
+						totalEffectiveValue += _pixelValue;
+					}
+
+					if (totalEffectiveValue > THRESHOLD) {
+						*(outputBytePtr) = 0;
+						*(outputBytePtr + 1) = 0;
+						*(outputBytePtr + 2) = 0;
+					}
+					else {
+						*(outputBytePtr) = 255;
+						*(outputBytePtr + 1) = 255;
+						*(outputBytePtr + 2) = 255;
+					}
+
+					outputBytePtr += 4;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Calculates offsets and weights used in edge filtering.
+		/// </summary>
+		void PrepareEdgeFilterOffsetsAndWeights() {
+			var edgeFilterWeights = new List<int> {
+				-1, -1, -1,
+				-1,  8, -1,
+				-1, -1, -1,
+			};
+
+			var areaBox = new Rectangle {
+				Origin = new KIP.Structs.Point { X = -1, Y = -1 },
+				Extent = new KIP.Structs.Point { X = 1, Y = 1 },
+			};
+
+			var edgeFilterOffsets = PrepareOffsets(areaBox, edgeFilterWeights.Count, FrameWidth);
+
+			var filteredPixelCount = edgeFilterWeights.Where(f => f != 0).Count();
+
+			EdgeFilterOffsets = new int[filteredPixelCount];
+			EdgeFilterWeights = new int[filteredPixelCount];
+
+			var j = 0;
+
+			for (var i = 0; i < edgeFilterWeights.Count; i++) {
+				if (edgeFilterWeights[i] == 0)
+					continue;
+
+				EdgeFilterWeights[j] = edgeFilterWeights[i];
+				EdgeFilterOffsets[j] = edgeFilterOffsets[i] * 4;
+
+				j++;
+			}
+		}
+
+		/// <summary>
+		/// A universal method for calculating all of the linear offsets for a given square area
+		/// </summary>
+		/// <exception cref="ArgumentException" />
+		int[] PrepareOffsets(Rectangle areaBox, int area, int stride, bool byteMultiplier = true) {
+			if (area % 2 == 0)
+				throw new ArgumentException("Odd sizes only.");
+
+			var offsets = new int[area];
+
+			var offset = 0;
+
+			for (var yOffset = areaBox.Origin.Y; yOffset <= areaBox.Extent.Y; yOffset++) {
+				for (var xOffset = areaBox.Origin.X; xOffset <= areaBox.Extent.X; xOffset++) {
+					offsets[offset] = (yOffset * stride) + xOffset;
+
+					if (byteMultiplier)
+						offsets[offset] = offsets[offset] * 4;
+
+					offset++;
+				}
+			}
+
+			return offsets;
+		}
+
+		/// <summary>
+		/// Precalculate pixel values
+		/// </summary>
+		void PreparePixelLayers(FrameDescription frameDescription) {
+			var frameWidth = frameDescription.Width;
+			var frameHeight = frameDescription.Height;
+			var pixelCount = frameDescription.LengthInPixels;
+			
+			var layers = Enum.GetValues(typeof(ELayer));
+
+			OverlayLayers = new Pixel[layers.Length][];
+
+			foreach (int layer in layers)
+				OverlayLayers[layer] = new Pixel[pixelCount];
+
+			Pixels = new Pixel[pixelCount];
+
+			var imageMidX = frameWidth / 2;
+			var imageMidY = frameHeight / 2;
+
+			for (var i = 0; i < pixelCount; i++) {
+				var y = i / frameWidth;
+				var x = i % frameWidth;
+
+				var aSq = Math.Pow(x - imageMidX, 2);
+				var bSq = Math.Pow(y - imageMidY, 2);
+				var cSq = Math.Sqrt(aSq + bSq);
+
+				Pixels[i].Location = new KIP.Structs.Point {
+					X = x,
+					Y = y,
+					Distance = cSq
+				};
+			}
+		}
+
+		enum ELayer {
+			FocalPoint,
+			Middles
+		}
+
+		/// <summary>
+		/// ImageProcessorService factory
+		/// </summary>
+		public static ImageProcessorService Create(FrameDescription frameDescription) {
+			var frameWidth = frameDescription.Width;
+			var frameHeight = frameDescription.Height;
+			var frameStride = frameWidth * 4;
+			var pixelCount = frameDescription.LengthInPixels;
+			var byteCount = frameDescription.LengthInPixels * 4;
+
+			var halfWidth = Convert.ToInt32(Math.Floor((double) FOCUSPARTWIDTH / 2));
+
+			var window = new Rectangle {
+				Origin = new KIP.Structs.Point { X = -halfWidth, Y = -halfWidth },
+				Extent = new KIP.Structs.Point { X = halfWidth, Y = halfWidth }
+			};
+
+			var focusPartArea = FOCUSPARTWIDTH * FOCUSPARTWIDTH; // 121
+
+			var service = new ImageProcessorService {
+				FrameWidth = frameWidth,
+				FrameStride = frameStride,
+				PixelCount = pixelCount,
+				OutputData = new byte[byteCount],
+				ColorFrameData = new byte[byteCount],
+				FrameChangedRect = new Int32Rect(0, 0, frameDescription.Width, frameDescription.Height)
+			};
+
+			service.FocusPartOffsets = service.PrepareOffsets(window, focusPartArea, frameWidth, false);
+			service.PrepareEdgeFilterOffsetsAndWeights();
+			service.PreparePixelLayers(frameDescription);
+
+			return service;
+		}
+	}
+}
