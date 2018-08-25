0ef25d179205d593f4d133aaba67ef7bf2df8179
Jim Yarbro <yarbro@outlook.com>
2018-08-25 12:39:27 +0200

    Added a new struct to be used by ContrastFilter

---
+++ KIP/Structs/LayerOffsets.cs
@@ -0,0 +1,13 @@
+﻿namespace KIP.Structs {
+	public struct LayerOffsets {
+		public int TL;
+		public int TC;
+		public int TR;
+		public int CL;
+		public int CC;
+		public int CR;
+		public int BL;
+		public int BC;
+		public int BR;
+	}
+}
